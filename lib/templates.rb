78a3e66a3cec605b73ec668b0e7651e695fd976c
shoshoi <shoshoiprop@gmail.com>
2019-04-07 22:07:00 +0900

    fix templates bug

--- lib/templates.rb
+++ lib/templates.rb
@@ -1,7 +1,7 @@
 module Templates
   def self.migration_file
     <<-EOS
-class ${class_name} < ActiveRecord::Migration[5.0]
+class Create${class_name} < ActiveRecord::Migration[5.0]
   def change
     create_table :${table_name} do |t|
 ${columns}
